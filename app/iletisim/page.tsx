d452ca7df767fe91565235ff29baffe3b949b752
Saki Celik <87222451+sakiceliks@users.noreply.github.com>
2025-09-23 01:31:59 +0300

    Update page.tsx

--- app/iletisim/page.tsx
+++ app/iletisim/page.tsx
@@ -237,6 +237,7 @@ export default function ContactPage() {
                 </div>
               ))}
             </div>
+            <ContactForm/>
           </div>
         </div>
       </section>
