c5f88d98e004f9fe66d52ae69c79e3f0284f4b8c
Saki Celik <87222451+sakiceliks@users.noreply.github.com>
2025-09-23 01:55:23 +0300

    Update ContactList.tsx

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -24,7 +24,7 @@ const ContactInfo = () => {
   ];
 
   return (
-    <div className="py-16 bg-gray-50 dark:bg-gray-800">
+    <div className=" bg-gray-50 dark:bg-gray-800">
       <div className="container mx-auto px-4">
         <div className="max-w-4xl mx-auto">
           {/* Header */}
